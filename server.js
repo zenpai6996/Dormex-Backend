601fd9e19db542208732edcb0a75d6123d2fda7b
SRB <souhardaroybarman05@gmail.com>
2026-02-17 21:00:29 +0530

    Host backend

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import app from "./src/app.js";
 dotenv.config();
+const PORT = process.env.PORT;
 
 mongoose
 	.connect(process.env.MONGO_URI)
 	.then(() => {
 		console.log("MongoDB Connected");
-		app.listen(process.env.PORT, () =>
-			console.log(`Server running on port ${process.env.PORT}`),
+		app.listen(PORT, "0.0.0.0", () =>
+			console.log(`Server running on port ${PORT}`),
 		);
 	})
 	.catch((err) => console.log(err));
+
+app.get("/", async (req, res) => {
+	res.status(200).json({
+		message: "Welcome to Dormex API",
+	});
+});
